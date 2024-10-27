b16c4558406e062a5ce8268e34a3a3816e31dbb2
sahaaditi24 <sahaaditi925@gmail.com>
2024-10-27 19:16:08 +0530

    Array

---
+++ AccenturePracticeQuestion/Palindrome.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        int rev = 0;
+        while(n>0){
+            rev = rev * 10 + n % 10;
+            n = n/ 10;
+        }
+        System.out.println("Reversed string:" + rev);
+
+    }
+    
+}
