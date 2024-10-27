9e865214af088fcee25589694c5db1c80450ddf1
sahaaditi24 <sahaaditi925@gmail.com>
2024-10-27 12:28:15 +0530

    array

---
+++ Array_Accenture/Intersection.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+
+public class Intersection {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no. of elemets in 1st array");
+        int n1 = sc.nextInt();
+        int arr1[] = new int[n1];
+        for(int i=0; i<n1; i++){
+           arr1[i] = sc.nextInt(); 
+        }
+        System.out.println("Enter the no. of elements in 2nd array");
+        int n2 = sc.nextInt();
+        int arr2[] = new int[n1];
+        for(int i=0; i<n2; i++){
+           arr2[i] = sc.nextInt(); 
+        }
+
+        int [] intersection = new int[n1];
+        for(int i=0; i<n1; i++){
+            for(int j=0; j<n2; j++){
+                if(arr1[i]== arr2[j]){
+                    intersection[i] = arr1[i];
+                }
+            }
+        }
+        for(int i=0; i<n1; i++){
+            System.out.println(intersection[i]);
+        }
+
+    }
+    
+
+}
