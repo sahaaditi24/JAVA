3fc2bcaba095616e1d1a5d79d26d0ad549f659c6
sahaaditi24 <sahaaditi925@gmail.com>
2024-10-29 21:08:02 +0530

    movie

--- StorybasedProblems/Movie.java
+++ StorybasedProblems/Movie.java
@@ -1,3 +1,19 @@
+import java.util.*;
+
 public class Movie {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of pizza's");
+        int pizza = sc.nextInt();
+        System.out.println("Enter no. of puffs");
+        int puffs = sc.nextInt();
+        System.out.println("Enter no. of cold drinks");
+        int drinks = sc.nextInt();
+        int price = 0;
+
+        price = pizza * 100 + puffs * 20 + drinks * 10;
+        System.out.println(price);
+
+    }
     
 }
