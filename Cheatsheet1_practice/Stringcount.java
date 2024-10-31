6b5159e06d02de28ed74c613573ea44001df2234
sahaaditi24 <sahaaditi925@gmail.com>
2024-10-31 13:04:00 +0530

    q1

---
+++ Cheatsheet1_practice/Stringcount.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Stringcount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String str = sc.next();
+        int l=str.length();
+        StringBuilder newstr = new StringBuilder();
+        int count =1;
+        
+        for(int i=0; i<l;i++){
+            if( i< l-1 && str.charAt(i)== str.charAt(i+1)){
+                count++;     
+            }
+            else{
+                newstr.append(str.charAt(i));
+                if(count>1){
+                    newstr.append(count);
+                }
+                count = 1;
+            }
+        }
+        System.out.println(newstr);
+    }
+}
