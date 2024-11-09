91ef7ca1315c22e4a5798a661974890121e2dd06
sahaaditi24 <sahaaditi925@gmail.com>
2024-11-09 12:51:02 +0530

    commit

--- TCSPracticeQuestions/Push0packetsatend.java
+++ TCSPracticeQuestions/Push0packetsatend.java
@@ -10,7 +10,24 @@ public class Push0packetsatend {
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
+        int pos =0; 
 
+        for(int i=0; i<n; i++){
+            if(arr[i]!=0){
+                 arr[pos] = arr[i];
+                 pos++;
+            }
+        
+        }
+        while(pos<n){
+            arr[pos] = 0;
+            pos++;
+        }
+        System.out.println("The new array is");
+        for(int i=0; i<n; i++){
+            System.out.println(arr[i]);
+        }
         
     }
+
 }
