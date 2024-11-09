08a9886a10835b5af918f91d234a7194bf194b02
sahaaditi24 <sahaaditi925@gmail.com>
2024-11-10 00:05:24 +0530

    commit

--- IBM_CodingQuestions/countandconverttostring.java
+++ IBM_CodingQuestions/countandconverttostring.java
@@ -5,5 +5,18 @@ public class countandconverttostring {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter String");
         String str = sc.next();
+        StringBuilder newstr = new StringBuilder();
+        int l = str.length();
+        for(int i=0; i<l-1; i+=2){
+            char ch = str.charAt(i);
+            int count = str.charAt(i+1)-'0';
+            if(count > 0){
+                for(int j=0; j<count; j++){
+                    newstr.append(ch);
+                }
+            }
+        }
+        System.out.println("New string : " + newstr);
+
     }
 }
