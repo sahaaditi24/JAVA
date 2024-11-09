db814f2e76d2907f2832b1cc5859326414d714c8
sahaaditi24 <sahaaditi925@gmail.com>
2024-11-09 23:10:15 +0530

    commit

---
+++ IBM_CodingQuestions/Stringcount.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Stringcount {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string");
+        String str = sc.next();
+        int l = str.length();
+        StringBuilder newstr = new StringBuilder();
+        int count =0;
+
+        for(int i=0; i<l; i++){
+            if(i<l-1 && str.charAt(i)== str.charAt(i+1)){
+                
+            }
+        }
+
+
+    }
+}
