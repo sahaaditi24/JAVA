0887e9a310f4246e5a61dffd3e15ad691d228569
sahaaditi24 <sahaaditi925@gmail.com>
2024-11-09 23:16:06 +0530

    commit

--- IBM_CodingQuestions/Stringcount.java
+++ IBM_CodingQuestions/Stringcount.java
@@ -7,11 +7,18 @@ public class Stringcount {
         String str = sc.next();
         int l = str.length();
         StringBuilder newstr = new StringBuilder();
-        int count =0;
+        int count =1;
 
         for(int i=0; i<l; i++){
             if(i<l-1 && str.charAt(i)== str.charAt(i+1)){
-                
+                count++;  
+            }
+            else{
+                newstr.append(str.charAt(i));
+                if(count> 1){
+                    newstr.append(count);
+                }
+                count =1;
             }
         }
 
