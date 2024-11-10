86081c6cc4df57f676317f72411727f5044441bc
sahaaditi24 <sahaaditi925@gmail.com>
2024-11-10 11:49:12 +0530

    commit

---
+++ IBM_CodingQuestions/Findmissingelement.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Findmissingelement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no. of elements in array");
+        int l = sc.nextInt();
+        int arr[] = new int[l];
+        
+
+
+    }
+}
