3c4fed637389b74cdb89264e78dbf525c7af9339
sahaaditi24 <sahaaditi925@gmail.com>
2024-11-10 13:08:35 +0530

    commit

--- IBM_CodingQuestions/Findmissingelement.java
+++ IBM_CodingQuestions/Findmissingelement.java
@@ -6,8 +6,32 @@ public class Findmissingelement {
         System.out.println("Enter the no. of elements in array");
         int l = sc.nextInt();
         int arr[] = new int[l];
-        
+        System.out.println("Enter elements of array");
+        for(int i=0; i<l; i++){
+            arr[i] = sc.nextInt();
+        }
+        int max= arr[0];
 
+        for(int i=1; i<l; i++){
+            if(max < arr[i]){
+                max= arr[i];
+            }
+        }
+        int temp = 0;
+        for(int i=0; i<l-1; i++){
+            if(arr[i]> arr[i+1]){
+                temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+        int output = 0;
+        for(int i=0;i<max; i++){
+            if(arr[i]!= i+1){
+                output = i+1;
+            }
+        }
+        System.out.println("The missing no. is "+output);
 
     }
 }
