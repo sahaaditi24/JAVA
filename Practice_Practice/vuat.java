4ef66695d2fed63349a24dce622f4f406f4f926c
sahaaditi24 <sahaaditi925@gmail.com>
2024-11-15 00:42:39 +0530

    commit

---
+++ Practice_Practice/vuat.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class vuat {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the value of V");
+        int v = sc.nextInt();
+        System.out.println("Enter the value of a");
+        int a = sc.nextInt();
+        System.out.println("Enter the value of t");
+        int t = sc.nextInt();
+        int u = v - a* t;
+        System.out.println("The output is:" + u);
+
+    }
+}
