f1eb17a6f8c0bcfd53472f23593422bd398e7425
sahaaditi24 <sahaaditi925@gmail.com>
2024-11-20 00:33:53 +0530

    commit

--- Practice_Practice/Sum_of_primefromarray.java
+++ Practice_Practice/Sum_of_primefromarray.java
@@ -6,24 +6,33 @@ public class Sum_of_primefromarray {
         System.out.println("Enter the no. of elements");
         int n = sc.nextInt();
         int arr[]= new int[n];
-        System.out.println("Enter no. of elements");
+        System.out.println("Enter no. of elements in array");
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
         int count =0;
         int sum =0;
-        for(int i=0; i<n-1; i++){
-            for(int j=2; j<n; j++){
-                if(arr[i] % j == 0){
-                   count++; 
-                }
-                if(count == 0){
-                    sum = sum + arr[i];
-                }
-            }
-            
-        }
 
-        System.out.println(" The sum is:" + sum);
+        for(int i=0; i<n; i++){
+            if(isprime(arr[i])){
+                sum = sum + arr[i];
+            }
+        }
+        System.out.println("The sum is " + sum);
+
     }
+
+    public static boolean isprime(int n){
+        if(n<= 1){
+            return false;
+        }
+        for(int i=2; i<n ; i++){
+            if (n%i == 0){
+                return false;
+            }
+
+        }
+        return true;
+    }
+    
 }
