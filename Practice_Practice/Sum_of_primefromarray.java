853bbcd38bd35e12b04fcfdd25a50a8ac72b8678
sahaaditi24 <sahaaditi925@gmail.com>
2024-11-20 01:19:47 +0530

    commit

--- Practice_Practice/Sum_of_primefromarray.java
+++ Practice_Practice/Sum_of_primefromarray.java
@@ -10,7 +10,6 @@ public class Sum_of_primefromarray {
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
-        int count =0;
         int sum =0;
 
         for(int i=0; i<n; i++){
