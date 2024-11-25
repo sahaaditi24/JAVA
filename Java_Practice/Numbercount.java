523d8b0a650d15430bbd41507a900004f32dd7a8
sahaaditi24 <sahaaditi925@gmail.com>
2024-11-25 23:43:42 +0530

    commit

--- Java_Practice/Numbercount.java
+++ Java_Practice/Numbercount.java
@@ -3,5 +3,29 @@ import java.util.*;
 public class Numbercount {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        boolean []visited = new boolean[n];
+
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                int count =1;
+                for(int j=i+1; j<n; j++){
+                    if(arr[i]== arr[j]){
+                        count++;
+                        visited[j] = true;
+                    }
+                }
+                System.out.println(arr[i]+ "occurs" + count+ "times");
+               
+
+            }
+            
+        }
     }
 }
