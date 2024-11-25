e52e1e24e062c148f76694cd8508cae18c74cd34
sahaaditi24 <sahaaditi925@gmail.com>
2024-11-26 00:09:35 +0530

    commit

--- Java_Practice/Removehash.java
+++ Java_Practice/Removehash.java
@@ -3,6 +3,22 @@ import java.util.*;
 public class Removehash {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter str");
+        String str = sc.next();
+        StringBuilder hash = new StringBuilder();
+        StringBuilder letters = new StringBuilder();
         
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i) == '#'){
+                hash.append(str.charAt(i));
+            }
+            else{
+                letters.append(str.charAt(i));
+            }
+        }
+        System.out.println(letters.toString());
+
+
+
     }
 }
