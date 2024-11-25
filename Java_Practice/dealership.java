5f9ade5cbacafc279475f1338970df2fd02de3ff
sahaaditi24 <sahaaditi925@gmail.com>
2024-11-25 23:27:02 +0530

    commit

--- Java_Practice/dealership.java
+++ Java_Practice/dealership.java
@@ -3,8 +3,18 @@ import java.util.*;
 public class dealership {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter n");
+        System.out.println("Enter no. of dealers");
         int n = sc.nextInt();
-        
+        int result= 0;
+        for(int i=0; i<n; i++){
+            System.out.println("Enter cars");
+            int c = sc.nextInt();
+            System.out.println("Enter bikes");
+            int b = sc.nextInt();
+            result = c*4 + b*2;
+            System.out.println(result);
+
+        }
+
     }
 }
