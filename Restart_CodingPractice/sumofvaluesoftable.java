00e2197e9b843f95892a88ded8141cd7ad8c226b
sahaaditi24 <sahaaditi925@gmail.com>
2025-02-08 22:59:23 +0530

    silly mistake

--- Restart_CodingPractice/sumofvaluesoftable.java
+++ Restart_CodingPractice/sumofvaluesoftable.java
@@ -4,10 +4,10 @@ public class sumofvaluesoftable {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the target");
-        int t= sc.nextInt();
+        int T= sc.nextInt();
         int sum =0;  
         for(int i=1; i<11; i++){
-            sum += t*i;
+            sum += T*i;
         }
         System.out.println(sum);
     }
